ba1b7b3b7a2aba258fe530cf68d8ed103d4dfd44
Piper <77084364+pipemm@users.noreply.github.com>
2023-12-25 23:53:25 +0800

    Update Simple.sh

--- Test/Simple.sh
+++ Test/Simple.sh
@@ -4,7 +4,18 @@ echo 'Print Working Directory: '
 docker run "${TEST_IMAGE}" pwd
 echo
 
+echo 'Bourne Again Shell: '
+docker run "${TEST_IMAGE}" bash --version
+echo
+
 echo 'List information about the FILEs: '
 docker run "${TEST_IMAGE}" ls --version
-docker run "${TEST_IMAGE}" ls --all --almost-all -l --recursive --time-style=full-iso
+echo
+
+echo 'AWK: '
+docker run "${TEST_IMAGE}" awk --version
+echo
+
+echo 'Stream EDitor: '
+docker run "${TEST_IMAGE}" sed --version
 echo
