3bc40f3d78c7019ceb92f39a17c0826db3d7fe0e
Piper <77084364+pipemm@users.noreply.github.com>
2024-01-20 22:45:04 +0800

    update tests

--- Test/01_Simple.sh
+++ Test/01_Simple.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
+echo 'print working directory: '
 docker run "${TEST_IMAGE}" 'pwd'
+
+echo 'Python 3: '
 docker run "${TEST_IMAGE}" 'python3' '--version'
--- Test/02_AWS_Test.sh
+++ Test/02_AWS_Test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+echo 'AWS CLI:'
 docker run "${TEST_IMAGE}" 'aws' '--version'
-docker run "${TEST_IMAGE}" 'python3' '-m' 'pip' '--version'
+
+echo 'boto3:'
 docker run "${TEST_IMAGE}" 'python3' '-c' 'import boto3; print(boto3.__version__);'
