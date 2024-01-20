39f4fe5d30eb39b68dd8d66ad8939793402afb0e
Piper <77084364+pipemm@users.noreply.github.com>
2024-01-20 20:50:25 +0800

    aws help

--- Test/02_AWS_Test.sh
+++ Test/02_AWS_Test.sh
@@ -2,10 +2,12 @@
 
 docker run "${TEST_IMAGE}" 'python3' '-m' 'pip' '--version'
 docker run "${TEST_IMAGE}" 'python3' '-c' 'import site; print(site.getsitepackages());'
-docker run "${TEST_IMAGE}" 'ls' '/usr/local/lib64/python3.9/site-packages'
-docker run "${TEST_IMAGE}" 'ls' '/usr/local/lib/python3.9/site-packages'
+## docker run "${TEST_IMAGE}" 'ls' '/usr/local/lib64/python3.9/site-packages'
+## docker run "${TEST_IMAGE}" 'ls' '/usr/local/lib/python3.9/site-packages'
+echo 'check /usr/lib64/python3.9/site-packages'
 docker run "${TEST_IMAGE}" 'ls' '/usr/lib64/python3.9/site-packages'
+echo 'check /usr/lib/python3.9/site-packages'
 docker run "${TEST_IMAGE}" 'ls' '/usr/lib/python3.9/site-packages'
 
 docker run "${TEST_IMAGE}" 'aws' '--version'
-docker run "${TEST_IMAGE}" 'aws' '--help'
+docker run "${TEST_IMAGE}" 'aws' 'help'
