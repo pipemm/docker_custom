319eef306d57f16429cfa0c9b1fa31ab70c5d087
Piper <77084364+pipemm@users.noreply.github.com>
2024-01-20 20:19:33 +0800

    added tests

--- Test/02_AWS_Test.sh
+++ Test/02_AWS_Test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 docker run "${TEST_IMAGE}" 'python3' '-m' 'pip' '--version'
+docker run "${TEST_IMAGE}" 'python3' 'site.getsitepackages();'
+docker run "${TEST_IMAGE}" 'python3' 'import site; site.getsitepackages();'
 docker run "${TEST_IMAGE}" 'aws' '--version'
