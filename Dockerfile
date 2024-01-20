098eefe8ef1a5191dc2d546573d6a5782fa6a630
Piper <77084364+pipemm@users.noreply.github.com>
2024-01-20 22:06:20 +0800

    finalizing

--- Dockerfile
+++ Dockerfile
@@ -7,17 +7,21 @@ RUN yum install --assumeyes unzip \
   && ./aws/install --bin-dir /aws-cli-bin/
 
 FROM public.ecr.aws/amazonlinux/amazonlinux:2023 AS al2023
+
+## AWS CLI
 RUN yum install --assumeyes less groff \
   && yum clean all
 COPY --from=cli-installer /usr/local/aws-cli/ /usr/local/aws-cli/
 COPY --from=cli-installer /aws-cli-bin/       /usr/local/bin/
 
+## boto3 (python)
+RUN python3 -m ensurepip \
+  && python3 -m pip install --no-cache-dir boto3 \
+  && python3 -m pip uninstall --yes pip setuptools
+
 FROM scratch
 LABEL maintainer='244682510@qq.com'
 
 COPY --from=al2023 / /
-RUN python3 -m ensurepip \
-  && python3 -m pip install --no-cache-dir boto3 \
-  && python3 -m pip uninstall --yes pip setuptools
 
 CMD ['/bin/bash']
--- Test/02_AWS_Test.sh
+++ Test/02_AWS_Test.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/bash
 
-docker run "${TEST_IMAGE}" 'python3' '-m' 'pip' '--version'
-docker run "${TEST_IMAGE}" 'python3' '-c' 'import site; print(site.getsitepackages());'
-
-echo 'check /usr/local/lib64/python3.9/site-packages'
-docker run "${TEST_IMAGE}" 'ls' '/usr/local/lib64/python3.9/site-packages'
-echo 'check /usr/local/lib/python3.9/site-packages'
-docker run "${TEST_IMAGE}" 'ls' '/usr/local/lib/python3.9/site-packages'
-echo 'check /usr/lib64/python3.9/site-packages'
-docker run "${TEST_IMAGE}" 'ls' '/usr/lib64/python3.9/site-packages'
-echo 'check /usr/lib/python3.9/site-packages'
-docker run "${TEST_IMAGE}" 'ls' '/usr/lib/python3.9/site-packages'
-
 docker run "${TEST_IMAGE}" 'aws' '--version'
-## docker run "${TEST_IMAGE}" 'aws' 'help'
+docker run "${TEST_IMAGE}" 'python3' '-m' 'pip' '--version'
+docker run "${TEST_IMAGE}" 'python3' '-c' 'import boto3; print(boto3.__version__);'
